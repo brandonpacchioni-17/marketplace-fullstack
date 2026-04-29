5ff2ead8a20981b89b1fd18360f31df62027baf5
Brandon Pacchioni <paccbrandon80@gmail.com>
2026-04-28 22:46:47 -0500

    allow public access

--- products/views.py
+++ products/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import AllowAny 
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
