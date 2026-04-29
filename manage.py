7caf389e7da38d5fb4e634eab50a7c736245b671
Brandon Pacchioni <paccbrandon80@gmail.com>
2026-04-28 23:27:05 -0500

    fix manage.py

--- manage.py
+++ manage.py
@@ -7,20 +7,14 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed?"
         ) from exc
-    from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', '12345678')
     execute_from_command_line(sys.argv)
 
 
