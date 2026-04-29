64d4770415f54e770035cd5d7dadb1f7ef73684a
Brandon Pacchioni <paccbrandon80@gmail.com>
2026-04-28 23:21:39 -0500

    create superuser via migration

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -8,24 +9,18 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
-
-        # IMPORTANTE: importar después de cargar Django
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                'admin',
-                'admin@example.com',
-                '12345678'
-            )
-
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django..."
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
+    from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '12345678')
     execute_from_command_line(sys.argv)
 
 
